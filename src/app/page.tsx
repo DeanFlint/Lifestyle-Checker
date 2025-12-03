15d1cf8889c4047642d6a96c2679d7cce9302e01
Dean Flint <deanpf790@gmail.com>
2025-12-03 21:27:22 +0000

    API routing added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>T2 Lifestyle Checker</h1>
-        <p>Welcome to the T2 Lifestyle Checker application.</p>
+        <div className={styles.heading}>
+          <h1>T2 Lifestyle Checker</h1>
+          <p>Welcome to the T2 Lifestyle Checker application.</p>
+        </div>
         <Login />
       </main>
     </div>
